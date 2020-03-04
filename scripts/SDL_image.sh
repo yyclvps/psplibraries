28a8fcb5228bdb9a64304fa16d06a4e52cf57b93
yyclvps <yyclvps@hotmail.com>
2020-03-05 03:40:33 +0800

    SDL_image SDL_ttf

--- scripts/SDL_image.sh
+++ scripts/SDL_image.sh
@@ -2,5 +2,6 @@ test_deps_install SDL libpng jpeg
 
 download_and_extract http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz SDL_image-1.2.12
 apply_patch SDL_image-1.2.12-PSP
-LIBPNG_CFLAGS="$CFLAGS -I$(psp-config --psp-prefix)/include/libpng15/" LDFLAGS="-lpspirkeyb" run_autogen_build --disable-webp --with-sdl-prefix=$(psp-config --psp-prefix)
+sed -i 's/LIBS="$saved_LIBS"/LIBS="$saved_LIBS -lm"/g' configure.in
+LIBPNG_CFLAGS="$CFLAGS -I$(psp-config --psp-prefix)/include/libpng15/" LDFLAGS="-lpspirkeyb" run_autogen_build --disable-webp --with-sdl-prefix=$(psp-config --psp-prefix) 
 
--- scripts/SDL_ttf.sh
+++ scripts/SDL_ttf.sh
@@ -1,5 +1,6 @@
 test_deps_install SDL freetype
 download_and_extract http://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11.tar.gz SDL_ttf-2.0.11
 apply_patch SDL_ttf-2.0.11-PSP
+sed -i '139iLIBS="$LIBS -lm"' configure.in 
 run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) --with-freetype-prefix=$(psp-config --psp-prefix) --without-x
 
